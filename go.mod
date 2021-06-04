3c8462f899839390547cdbb0f68d4ac482803e27
lakshmi-236 <41097845+lakshmi-236@users.noreply.github.com>
2021-06-04 12:53:21 +0530

    Add files via upload

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/TutorialEdge/create-rest-api-in-go-tutorial
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.2
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,81 +1,79 @@
-
 // main.go
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "io/ioutil"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Book - Our struct for all books
 type Book struct {
-    Id      string  `json:"Id"`
-    Title   string `json:"Title"`
-    Author  string `json:"Author"`
+	Id     string `json:"Id"`
+	Title  string `json:"Title"`
+	Author string `json:"Author"`
 }
 
 var Books []Book
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(Books)
+	json.NewEncoder(w).Encode(Books)
 }
 
 func getBookById(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
+	vars := mux.Vars(r)
+	key := vars["id"]
 
-    for _, book := range Books {
-        if book.Id == key {
-            json.NewEncoder(w).Encode(book)
-        }
-    }
+	for _, book := range Books {
+		if book.Id == key {
+			json.NewEncoder(w).Encode(book)
+		}
+	}
 }
 
-
 func createNewBook(w http.ResponseWriter, r *http.Request) {
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var book Book 
-    json.Unmarshal(reqBody, &book)
-    Books = append(Books, book)
-    json.NewEncoder(w).Encode(book)
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var book Book
+	json.Unmarshal(reqBody, &book)
+	Books = append(Books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-    for index, book := range Books {
-        if book.Id == id {
-            Books = append(Books[:index], Books[index+1:]...)
-        }
-    }
+	for index, book := range Books {
+		if book.Id == id {
+			Books = append(Books[:index], Books[index+1:]...)
+		}
+	}
 
 }
 
 func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/books", getBooks)
-    myRouter.HandleFunc("/book", createNewBook).Methods("POST")
-    myRouter.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
-    myRouter.HandleFunc("/book/{id}", getBookById)
-    log.Fatal(http.ListenAndServe(":8081", myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/books", getBooks)
+	myRouter.HandleFunc("/book", createNewBook).Methods("POST")
+	myRouter.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
+	myRouter.HandleFunc("/book/{id}", getBookById)
+	log.Fatal(http.ListenAndServe(":8082", myRouter))
 }
 
 func main() {
-    Books = []Book{
-        Book{Id: "1", Title: "Book1", Author: "Author 1"},
-        Book{Id: "2", Title: "Book 2", Author: "Author 2"},
-        Book{Id: "3", Title: "Book 3", Author: "Author 3"},
-
-    }
-    handleRequests()
+	Books = []Book{
+		Book{Id: "1", Title: "Book1", Author: "Author 1"},
+		Book{Id: "2", Title: "Book 2", Author: "Author 2"},
+		Book{Id: "3", Title: "Book 3", Author: "Author 3"},
+	}
+	handleRequests()
 }
